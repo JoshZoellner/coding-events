3dfff0fd31737bff625bf25511d80ff2533a4a29
Josh Zoellner <Josh.Zoellner@gmail.com>
2020-08-05 01:47:45 -0400

    added form handler

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,27 +14,22 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-        @GetMapping
-        public String displayAllEvents(Model model) {
-            List<String> events = new ArrayList<>();
-            events.add("JavaScript");
-            events.add("Java");
-            events.add("Thymeleaf");
-            model.addAttribute("events", events);
-            return "events/index";
-        }
+    private static List<String> events = new ArrayList<>();
+
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        model.addAttribute("events", events);
+        return "events/index";
+    }
 
     @GetMapping("create")
     public String renderCreateEventForm(){
         return "events/create";
     }
 
-    @GetMapping("events/index")
-    public String renderFormMethodName(Model model) {
-
-        // Method code...
-
-        return "pathToTemplate";
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
     }
-
 }
