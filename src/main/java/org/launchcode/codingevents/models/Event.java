611d5c64e512a8d57b0c204bb1c98dee04a12a57
Josh Zoellner <Josh.Zoellner@gmail.com>
2020-08-17 15:27:55 -0400

    added unique ID to events

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -4,12 +4,17 @@ import java.util.Objects;
 
 public class Event {
 
+    private int id;
+    private static int nextID = 1;
+
     private String name;
     private String description;
 
     public Event(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextID;
+        nextID++;
     }
 
     public String getName() {
@@ -28,9 +33,25 @@ public class Event {
         this.description = description;
     }
 
+    public int getId() {
+        return id;
+    }
+
     @Override
     public String toString() {
         return name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
